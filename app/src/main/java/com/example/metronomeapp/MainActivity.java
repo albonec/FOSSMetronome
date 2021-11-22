68e998beb93f66fdd7c5aba182954229b7be2781
albonec <70042211+albonec@users.noreply.github.com>
2021-11-21 22:54:42 -0500

    interval calc added

--- app/src/main/java/com/example/metronomeapp/MainActivity.java
+++ app/src/main/java/com/example/metronomeapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.metronomeapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,12 +37,13 @@ public class MainActivity extends AppCompatActivity {
         startBtn = (Button) findViewById(R.id.startButton);
         stopBtn = (Button) findViewById(R.id.stopButton);
 
+        //final MediaPlayer mediaPlayer = MediaPlayer.create(this);
+
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tempo = Integer.valueOf(TempoInput.getText().toString());
                 showToast(String.valueOf(tempo));
-                //add start code here
             }
         });
 
@@ -58,14 +60,18 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
+
     //Debug function to show text when necessary
     public void showToast(String text) { Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show(); }
 
+    //calculates interval at which to play the metronome tick based on tempo in BPM units.
+    public double calcInterval(int tempo) {
+        double interval = 60 / tempo;
+        return interval;
+    }
 }
